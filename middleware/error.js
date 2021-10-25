6a491c2a9b0582b65d17d97bc91ce2b65951af4b
younusraza <younusraza909@gmail.com>
2021-10-24 20:52:39 -0700

    Handling Mongoose Error Validation

--- middleware/error.js
+++ middleware/error.js
@@ -7,11 +7,27 @@ const ErrorHandler = (err, req, res, next) => {
     let error = { ...err }
     error.message = err.message
 
+
+
     // Error Handling For Bad Object Id Mongoose
     if (err.name === 'CastError') {
         const message = `Bootcamp not found with this Id ${error.value}`
         error = new ErrorResponse(message, 404)
     }
+
+    // Mongoose Duplicate Key Error
+    // Code 11000 is for duplicate key
+    if (err.code === 11000) {
+        const message = `Duplicate field value entered`
+        error = new ErrorResponse(message, 400)
+    }
+
+    // validation errror For Required Field
+    if (err.name === 'ValidationError') {
+        const message = Object.values(err.errors).map(val => val.message)
+        error = new ErrorResponse(message, 400)
+    }
+
     res.status(error.statusCode || 500).json({
         success: false,
         error: error.message || "Server Error"
