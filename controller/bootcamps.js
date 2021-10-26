86eca00a7c74e4fef3973d12e891d08d93e806d4
younusraza <younusraza909@gmail.com>
2021-10-26 16:31:23 -0700

    Added Pagination

--- controller/bootcamps.js
+++ controller/bootcamps.js
@@ -14,7 +14,7 @@ exports.getBootcamps = asyncHandler(async (req, res, next) => {
     // Copy Request Query
     const reqQuery = { ...req.query }
     // Field To Exclude
-    const removeFields = ['select', 'sort']
+    const removeFields = ['select', 'sort', "page", 'limit']
     //Loop over remove Fields and delete them from query String
     removeFields.forEach(params => delete reqQuery[params])
     // Creating Query String
@@ -37,12 +37,37 @@ exports.getBootcamps = asyncHandler(async (req, res, next) => {
         query.sort('-createdAt')
     }
 
+    // Pagination
+    const page = parseInt(req.query.page, 10) || 1
+    const limit = parseInt(req.query.limit, 10) || 100
+    const startIndex = (page - 1) * limit
+    const endIndex = page * limit
+    const total = await Bootcamp.countDocuments()
+
+    query = query.skip(startIndex).limit(limit)
+
     //Executing Query
     const bootcamps = await query
+
+    // pagination
+    const pagination = {}
+    if (endIndex < total) {
+        pagination.next = {
+            page: page + 1,
+            limit
+        }
+    }
+    if (startIndex > 0) {
+        pagination.prev = {
+            page: page - 1,
+            limit
+        }
+    }
 
 
     res.status(200).json({
         success: true,
+        pagination,
         count: bootcamps.length,
         data: bootcamps
 
