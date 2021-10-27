175911fe74a99629ef6e68cf569fc640f346df1f
younusraza <younusraza909@gmail.com>
2021-10-26 19:25:38 -0700

    Making controller to get all course

---
+++ controller/courses.js
@@ -0,0 +1,27 @@
+const Course = require('../models/Course')
+const ErrorResponse = require('../utils/errorResponse')
+// Async Handler 
+const asyncHandler = require('../middleware/async')
+
+// @desc     Get All Courses
+// @Route    Get:/api/v1/courses
+// @Route    Get:/api/v1/bootcamps/:bootcampId/courses
+// @Access   Public
+
+exports.getCourses = asyncHandler(async (req, res, next) => {
+    let query;
+
+    if (req.params.bootcampId) {
+        query = Course.find({ bootcamp: req.params.bootcampId })
+    } else {
+        query = Course.find()
+    }
+
+    const courses = await query
+
+    res.status(200).json({
+        success: true,
+        count: courses.length,
+        data: courses
+    })
+})
