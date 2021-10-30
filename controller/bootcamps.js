7eee631db03df1ff6480ee8841987b57f02bf28c
younusraza <younusraza909@gmail.com>
2021-10-29 19:12:56 -0700

    ensuring that owner can update and delete a bootcamp

--- controller/bootcamps.js
+++ controller/bootcamps.js
@@ -66,15 +66,25 @@ exports.createBootcamp = asyncHandler(async (req, res, next) => {
 // @Access   Private
 exports.updateBootcamp = asyncHandler(async (req, res, next) => {
 
-    const bootcamp = await Bootcamp.findByIdAndUpdate(req.params.id, req.body, {
-        new: true,
-        runValidators: true
-    })
+    let bootcamp = await Bootcamp.findById(req.params.id)
     if (!bootcamp) {
         return next(
             new ErrorResponse(`Bootcamp not found with this Id ${req.params.id}`, 404)
         )
     }
+    //Make sure user is bootcamp owner
+    if (bootcamp.user.toString() !== req.user.id && req.user.role !== 'admin') {
+        return next(
+            new ErrorResponse(`User ${req.params.id} is not authorized to update this bootcamp`, 401)
+        )
+    }
+
+    bootcamp = await Bootcamp.findByIdAndUpdate(req.params.id, req.body, {
+        new: true,
+        runValidators: true
+    })
+
+
     res.status(200).json({
         success: true,
         data: bootcamp
@@ -91,11 +101,20 @@ exports.deleteBootcamp = asyncHandler(async (req, res, next) => {
     //we can use findbyIdandDelete but it will not provoke pre remove middleware of mongoose
     //so we will find document and will use remove method on it
     const bootcamp = await Bootcamp.findById(req.params.id)
+
     if (!bootcamp) {
         return next(
             new ErrorResponse(`Bootcamp not found with this Id ${req.params.id}`, 404)
         )
     }
+
+    //Make sure user is bootcamp owner
+    if (bootcamp.user.toString() !== req.user.id && req.user.role !== 'admin') {
+        return next(
+            new ErrorResponse(`User ${req.params.id} is not authorized to delete this bootcamp`, 401)
+        )
+    }
+
 
     await bootcamp.remove()
     res.status(200).json({
